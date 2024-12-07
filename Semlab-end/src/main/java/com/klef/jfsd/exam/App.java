3b864bf2a9d9344e6cb3113db6e142a87ce692a3
LUKKA ROSHINI <138683204+LUKKAROSHINI@users.noreply.github.com>
2024-12-07 08:17:55 +0530

    Update App.java

--- Semlab-end/src/main/java/com/klef/jfsd/exam/App.java
+++ Semlab-end/src/main/java/com/klef/jfsd/exam/App.java
@@ -5,14 +5,14 @@ import org.hibernate.cfg.Configuration;
 
 public class App {
   public static void main(String[] args) {
-    // Initialize SessionFactory
+  
     SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
     Session session = factory.openSession();
 
-    // Start transaction
+ 
     Transaction transaction = session.beginTransaction();
 
-    // Create Customer objects
+   
     Customer customer1 = new Customer();
     customer1.setName("Alice");
     customer1.setEmail("alice@example.com");
@@ -31,12 +31,12 @@ public class App {
     customer3.setAge(28);
     customer3.setLocation("Texas");
 
-    // Save the Customer objects to the database
+    
     session.save(customer1);
     session.save(customer2);
     session.save(customer3);
 
-    // Commit transaction
+   
     transaction.commit();
 
     System.out.println("Data inserted successfully!");
